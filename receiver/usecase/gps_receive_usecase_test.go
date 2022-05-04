cb791567181a74b181fed6408567bbbc58716df1
ktr03 <ktrsouth@gmail.com>
2022-05-04 15:12:02 +0900

    test: add gps receive usecase error case

--- receiver/usecase/gps_receive_usecase_test.go
+++ receiver/usecase/gps_receive_usecase_test.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
@@ -14,38 +15,71 @@ import (
 )
 
 func TestGpsReceiveUsecaseExecuteNormalCase(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	repository := mock.NewMockGpsReceiveRepository(ctrl)
-	mock := clock.NewMock()
-	usecase := NewGpsReceiveUsecase(repository, mock)
-
-	iteration := 2
-	duration := 1000 * time.Millisecond
 	date := time.Date(2022, 5, 3, 0, 9, 0, 0, time.Local)
-	expectedOutput := &model.Gps{Timestamp: date, Mode: 3, Lat: 30.11, Lon: 130.11, Alt: 30.11, Speed: 30.11}
-	var expectedErr error
+	duration := 1000 * time.Millisecond
 
-	context, cancel := context.WithCancel(context.Background())
-	eg, ctx := errgroup.WithContext(context)
-	ch := make(chan *model.Gps)
-
-	repository.EXPECT().Receive().Return(expectedOutput, expectedErr).Times(iteration)
-	eg.Go(func() error { return usecase.Execute(ctx, duration, ch) })
-
-	for i := 0; i < iteration; i++ {
-		gosched()
-		mock.Add(duration)
-
-		output := <-ch
-		assert.Exactly(t, expectedOutput, output)
+	tests := []struct {
+		name              string
+		returnErr         error
+		expectedOutput    *model.Gps
+		expectedErr       error
+		expectedCallTimes int
+	}{
+		{
+			"normal case",
+			nil,
+			&model.Gps{Timestamp: date, Mode: 3, Lat: 30.11, Lon: 130.11, Alt: 30.11, Speed: 30.11},
+			nil,
+			2,
+		},
+		{
+			"error case",
+			errors.New("error occurred"),
+			nil,
+			errors.New("failed to receive GPS data"),
+			1,
+		},
 	}
 
-	cancel()
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
-	if err := eg.Wait(); err != nil {
-		t.Fatalf("error is not expected but received: %v", err)
+			repository := mock.NewMockGpsReceiveRepository(ctrl)
+			mock := clock.NewMock()
+			usecase := NewGpsReceiveUsecase(repository, mock)
+
+			context, cancel := context.WithCancel(context.Background())
+			eg, ctx := errgroup.WithContext(context)
+			ch := make(chan *model.Gps)
+
+			repository.EXPECT().Receive().Return(tt.expectedOutput, tt.returnErr).Times(tt.expectedCallTimes)
+			eg.Go(func() error { return usecase.Execute(ctx, duration, ch) })
+
+			for i := 0; i < tt.expectedCallTimes; i++ {
+				gosched()
+				mock.Add(duration)
+
+				if tt.expectedErr == nil {
+					output := <-ch
+					assert.Exactly(t, tt.expectedOutput, output)
+				}
+			}
+
+			cancel()
+
+			if err := eg.Wait(); err != nil {
+				if tt.expectedErr != nil {
+					assert.Contains(t, err.Error(), tt.expectedErr.Error())
+				} else {
+					t.Fatalf("error is not expected but received: %v", err)
+				}
+			} else {
+				assert.Exactly(t, tt.expectedErr, nil, "error is expected but received nil")
+			}
+		})
 	}
 }
 
