860d6c6426a0fa99a7e98101754954c3b71f04a9
ktr03 <ktrsouth@gmail.com>
2022-05-19 23:14:53 +0900

    transfer script

--- rsync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")" || exit 1
-dir="$(basename "$(pwd)")"
-
-# define Host pi at ~/.ssh/config to specify copy target
-fswatch -o . | xargs -I{} rsync -av --delete . pi:work/"${dir}"
